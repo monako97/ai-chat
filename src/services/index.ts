057b58504ad7bfa75f46cb7854808f392b229ab7
Moneko <poi.nyaa@qq.com>
2024-11-12 08:35:12 +0000

    fix: lint

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,7 +8,9 @@ extend({
       req.headers!.Authorization = `Bearer ${global.OPENROUTER_API_KEY}`;
     },
     httpError(res) {
-      global.error = res.responseType === 'text' ? JSON.parse(res.response) : res.response;
+      const resp = res.responseType === 'text' ? JSON.parse(res.response) : res.response;
+
+      global.error = resp.error;
     },
   },
   prefixUrl: 'https://openrouter.ai/api/v1',
