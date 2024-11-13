45409fb04ae841fd12713d858b246c113dce7d6c
Moneko <poi.nyaa@qq.com>
2024-11-13 19:28:45 +0800

    Update global.ts

--- src/store/global.ts
+++ src/store/global.ts
@@ -6,7 +6,7 @@ import '@/services';
 const OPENROUTER_API_KEY = 'OPENROUTER_API_KEY';
 const DEFAULT_OPENROUTER_KEY = persistence.load<string>(
   OPENROUTER_API_KEY,
-  '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',
+  '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',
 );
 
 sso.config({
